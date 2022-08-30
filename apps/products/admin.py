e1614c1698773f63a890d65561c4e64d914e0670
artio <artiom.carter.03@mail.ru>
2022-08-30 12:03:46 +0300

    Add models

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.products.models import (
+    Shop,
+    Product,
+    ProductShop,
+    Attachment,
+    Category,
+    Comment,
+)
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'price', 'rating')
+
+
+@admin.register(ProductShop)
+class ProductShopAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'available', 'shop', 'product')
+
+
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'file_url', 'extension', 'file_size')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'parent', 'created_at')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'shop', 'email', 'username', 'rating')
