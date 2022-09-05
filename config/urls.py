cde51a91a7b6bf5bc322ed8370b36e1574499b73
Daniel Cuznetov <danielcuznetov04@gmail.com>
2022-09-05 11:08:10 +0300

    Redact imports

--- config/urls.py
+++ config/urls.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import (
+    path,
+    include
+)
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 # noinspection PyUnresolvedReferences
