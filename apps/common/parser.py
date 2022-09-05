428a3a0a7666f763026374551ca2f044a8a6f5bb
artio <artiom.carter.03@mail.ru>
2022-09-05 15:20:47 +0300

    Update parser

--- apps/common/parser.py
+++ apps/common/parser.py
@@ -1,8 +1,9 @@
 import json
+from json import JSONDecodeError
 
-from fake_useragent import UserAgent
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 
 class Parsing:
@@ -15,8 +16,9 @@ class Parsing:
 
     def __init__(self):
         self.category = {}
-        self.table_goods = {}
+        self.table_goods = []
         self.page = 1
+        self.link = None
 
     @staticmethod
     def logging(message, data=None):
@@ -25,8 +27,8 @@ class Parsing:
     def get_category_link(self):
         url = 'https://enter.online/'
 
-        html = requests.get(url, headers=self.headers)
-        soup = BeautifulSoup(html.text, 'html.parser')
+        response = requests.get(url, headers=self.headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
         categories = soup.select('.uk-position-top-left > .uk-nav.tm-mobile-menu-nav > .blue')
         for main_category in categories:
             if main_category.get('href') is None:
@@ -46,15 +48,36 @@ class Parsing:
 
             for key, value in zip(data.keys(), data.values()):
                 while True:
-                    link = value['category_link']
-                    html = requests.get(f'{link}?page={self.page}', headers=self.headers, allow_redirects=False)
-                    soup = BeautifulSoup(html.text, 'html.parser')
+                    self.link = value['category_link']
+                    response = requests.get(
+                        f'{self.link}?page={self.page}',
+                        headers=self.headers,
+                        allow_redirects=False
+                    )
+
+                    if response.is_redirect:
+                        redirected_link = response.headers['Location']
+                        self.link = redirected_link
+                        response = requests.get(
+                            f'{self.link}?page={self.page}',
+                            headers=self.headers,
+                            allow_redirects=False
+                        )
+
+                    self.logging(
+                        'Added goods card',
+                        f'Status code: {response.status_code} | Page: {self.page} | URL: {response.url}'
+                    )
+                    soup = BeautifulSoup(response.text, 'html.parser')
                     goods = soup.select('.product-card > div > .grid-item')
                     if not goods:
                         self.page = 1
                         break
                     for good in goods:
+                        not_available = good.attrs.get('data-stock')
                         html_price = good.select('.grid-price-cart > .grid-price > .price')
+                        html_price_discount = good.select('.grid-price-cart > .grid-price > .price-new')
+                        discount_price = ''.join([price.text for price in html_price_discount])
                         price = ''.join([price.text for price in html_price])
                         html_title = good.select('div > a > .product-title')
                         title = ''.join([i.text for i in html_title])
@@ -67,18 +90,21 @@ class Parsing:
                             'link': link,
                             'title': title,
                             'description': description,
-                            'price': price
+                            'price': price if price else discount_price,
+                            'available': False if not_available else True
                         }
-                        self.table_goods[key] = dictionary
-                    with open('grid_items.json', 'a', encoding='utf-8') as fp:
-                        fp.write(json.dumps(self.table_goods))
-                    self.page += 1
-                    self.logging(
-                        'Added good card', f'Status code: {html.status_code} | Page: {self.page - 1} | URL: {html.url}'
-                    )
-
-
-
+                        self.table_goods.append({key: dictionary})
+                    try:
+                        with open('grid_items1.json') as fp:
+                            old_table_goods = json.load(fp)
+                        old_table_goods.extend(self.table_goods)
+                        with open('grid_items1.json', 'w', encoding='utf-8') as fp:
+                            fp.write(json.dumps(old_table_goods).replace(r'\n', ''))
+                    except JSONDecodeError:
+                        with open('grid_items1.json', 'w', encoding='utf-8') as fp:
+                            fp.write(json.dumps(self.table_goods).replace(r'\n', ''))
+                    finally:
+                        self.page += 1
 
 
 if __name__ == '__main__':
