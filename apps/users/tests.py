9c9f63b7dcabd1cbd123c4609a0ac471d5356897
artio <artiom.carter.03@mail.ru>
2022-09-27 09:25:22 +0300

    Update

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -60,4 +60,16 @@ class UserTestCase(APITestCase):
             '/token/refresh',
             data=data
         )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_user_list(self):
+        response = self.client.get(
+            '/users'
+        )
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
