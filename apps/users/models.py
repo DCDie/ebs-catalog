dd95f58e8af89d4a1e244b5954b29ce3a488f536
artio <artiom.carter.03@mail.ru>
2022-09-27 17:32:55 +0300

    Add custom auth

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,21 +1,43 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-from django.utils.translation import gettext_lazy as _
-
 __all__ = [
-    'CustomUser'
+    'CustomUser',
+    'CustomUserManager'
 ]
 
 
-class CustomUser(AbstractUser):
-    username = models.EmailField(
-        _('email address'),
-        blank=False,
-        unique=True
-    )
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email=None, password=None, is_staff=False, is_superuser=False):
+        if not email:
+            raise ValueError('User must contain email')
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.is_active = True
+        user.is_staff = is_staff
+        user.is_superuser = is_superuser
+        user.save()
+        return user
 
-    USERNAME_FIELD = 'username'
+    def create_superuser(self, email, password):
+        user = self.create_user(
+            email=email,
+            password=password,
+            is_staff=True,
+            is_superuser=True
+        )
+        user.save()
+        return user
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField('email address', blank=False, unique=True)
+    username = None
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     class Meta:
