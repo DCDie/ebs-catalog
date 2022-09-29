098a641842d5f5b2efce26b67756b8f436a56bda
Nichita <nichita.cuznetov nichita.cuznetov@mail.ebs-integrator.com>
2022-09-29 16:21:02 +0300

    added ordering filtrations and search fields

--- config/settings.py
+++ config/settings.py
@@ -82,8 +82,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
-        'apps.common.filters.DefaultSearchFilter',
         'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
